40ed9b54d06eb1fe92564bdd775693007c441dae
codershona <falgunimst95@gmail.com>
2023-03-05 18:43:24 +0000

    worked on created models and added columns

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,8 @@
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -9,12 +12,34 @@ export class MainViewComponent implements OnInit {
 
   constructor() { }
 
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Some of my own random idea",
+      "This could be another random messy ideas",
+      "I am building an Kanban Apps"
+    ]),
+    new Column('Research', [
+      "Some of my own random research 2",
+      "This could be another random messy research 2",
+      "I am building an Kanban Apps 2"
+    ]),
+    new Column('Todo', [
+      'Get to work', 
+      'Pick up groceries', 
+      'Go home', 
+      'Fall asleep'
+    ]),
+    new Column('Completed', [
+      'Get up', 
+      'Brush teeth', 
+      'Take a shower', 
+      'Check e-mail', 
+      'Walk dog'
+    ]),
+  ]);
+
   ngOnInit() {
   }
-
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
